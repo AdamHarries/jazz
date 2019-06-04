a5cbe8c33fa860923dc2d70b49103bdde27548b4
Adam Harries <adam.harries@codeplay.com>
2019-06-04 17:32:27 +0100

    fix deployment script

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build -t createbooks .
-docker run -it --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined createbooks "Hello world"
+docker run -it --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined createbooks
+docker cp (docker ps -alq):/home/root/books ./books
