306a528905407b8f6c03e844793a735d4e3ab353
Adam Harries <adam.harries@codeplay.com>
2019-08-23 16:54:08 +0100

    Move back to drafts, and copy directory contents, rather than directory

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker build -t createbooks .
 docker run -it --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined createbooks Manual
-docker cp $(docker ps -alq):/home/root/books .
-docker cp $(docker ps -alq):/home/root/draft_books .
+mkdir -p books
+docker cp $(docker ps -alq):/home/root/books/. ./books
+docker cp $(docker ps -alq):/home/root/draft_books/. ./books
