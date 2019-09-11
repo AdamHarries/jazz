cc397e8970af81d2fa1255fe3f137749909fa88a
Adam Harries <adam.harries@codeplay.com>
2019-09-11 14:50:41 +0100

    Rework the web stuff a little, and tweak the upload script

--- upload_books.py
+++ upload_books.py
@@ -5,8 +5,9 @@ import boto3
 s3 = boto3.client('s3')
 
 bucket_name = 'stompin-at-summerhall-resources'
+print("Uploading " + "resources/index.html" + " to " + bucket_name + "/" + "index.html")
+s3.upload_file("resources/index.html", bucket_name, "index.html", ExtraArgs={'ContentType': "text/html", 'ACL':"public-read"})
 for name in ['eb', 'bb', 'c']:
     filename = 'books/' + name + ".pdf"
     print("Uploading " + filename + " to " + bucket_name + "/" + filename)
-    response = s3.upload_file(filename, bucket_name, filename)
-    print(str(response))
+    s3.upload_file(filename, bucket_name, filename, ExtraArgs={'ContentType':"application/pdf", 'ACL':"public-read"})
